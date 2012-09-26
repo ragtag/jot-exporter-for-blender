a038b851c12edf318074955dafa8429fe8681005
Ragnar Brynjúlfsson <ragtag@ragtag.net>
2012-09-26 22:28:41 +0000

    Fixed bug with UVs. Fixed bug with creases. Added a todo list. Exporter now makes .jot files that jot can read, though not perfectly correctly.

--- io_export_jot.py
+++ io_export_jot.py
@@ -67,13 +67,9 @@ class ExportJot(bpy.types.Operator, ExportHelper):
         self.file.write('  }}\n')
         # Export creased edges.
         self.file.write('       creases {{ ')
-        has_creases = False
         for edge in obj.data.edges:
             if edge.crease > 0.0:
                 self.file.write('{ %s %s }' % ( edge.vertices[0], edge.vertices[1] ) )
-                has_creases = True
-        if not has_creases:
-            self.file.write(' {} ')
         self.file.write('  }}\n')
         # Export UV's.
         try:
@@ -88,20 +84,22 @@ class ExportJot(bpy.types.Operator, ExportHelper):
                 else:
                     self.file.write('{ %s { %s %s }{ %s %s }{ %s %s } }' % ( face_count, uvs[0][0], uvs[0][1], uvs[1][0], uvs[1][1], uvs[2][0], uvs[2][1] ) )
                     face_count += 1
-                    self.file.write('{ %s { %s %s }{ %s %s }{ %s %s }' % ( face_count, uvs[0][0], uvs[0][1], uvs[2][0], uvs[2][1], uvs[3][0], uvs[3][1] ) )
+                    self.file.write('{ %s { %s %s }{ %s %s }{ %s %s } }' % ( face_count, uvs[0][0], uvs[0][1], uvs[2][0], uvs[2][1], uvs[3][0], uvs[3][1] ) )
                     face_count += 1
-            self.file.write('}\n')
+            self.file.write(' }}\n')
         except:
             # Object lacks UV's. 
             self.file.write('{} ')
-        self.file.write(' }}\n')
+        self.file.write('   }\n')
+        self.file.write(' }\n')
         self.file.write('}\n')
         self.file.write('CREATE { %s }' % obj.name)
         # TEXBODY END
 
         # CAMERA & VIEW
         # Export Camera
-        # Todo! Aim at location, up vector and field of view.
+        # TODO! Aim at location, up vector and field of view.
+        # TODO! Camera is seemingly pointing in the right direction, but model is facing differently. Find out what axis jot is using for up.
         cam = bpy.data.scenes[0].camera
         self.file.write('\nCHNG_CAM {\n')
         self.file.write('{ %s %s %s }' % ( cam.location[0], cam.location[1], cam.location[2] ) )
@@ -110,21 +108,21 @@ class ExportJot(bpy.types.Operator, ExportHelper):
         self.file.write('{ 0.0 0.0 0.0 }')
         self.file.write(' 0.2 1 2.25 }\n')
         # Set Windows size TODO! Don't know if this is needed.
-        self.file.write('CHNG_WIN { 34 480 580 }\n')
+        #self.file.write('CHNG_WIN { 30 34 480 580 }\n')
         # Set the View TODO! Don't know if this is needed, and insert correct data.
-        self.file.write('CHNG_VIEW {\n')
-        self.file.write('  VIEW {\n')
-        self.file.write('    view_animator {\n')
-        self.file.write('      Animator {\n')
-        self.file.write('        fps            -1\n')
-        self.file.write('        start_frame    -1\n')
-        self.file.write('        end_frame      -1\n')
-        self.file.write('        name { NULL_STR } \n')
-        self.file.write('      }\n')
-        self.file.write('    }\n')
-        self.file.write('    view_data_file { NULL_STR }\n')
-        self.file.write('  }\n')
-        self.file.write('}\n')
+        #self.file.write('CHNG_VIEW {\n')
+        #self.file.write('  VIEW {\n')
+        #self.file.write('    view_animator {\n')
+        #self.file.write('      Animator {\n')
+        #self.file.write('        fps            -1\n')
+        #self.file.write('        start_frame    -1\n')
+        #self.file.write('        end_frame      -1\n')
+        #self.file.write('        name { NULL_STR } \n')
+        #self.file.write('      }\n')
+        #self.file.write('    }\n')
+        #self.file.write('    view_data_file { NULL_STR }\n')
+        #self.file.write('  }\n')
+        #self.file.write('}\n')
                 
 def menu_func(self, context):
     self.layout.operator(ExportJot.bl_idname, text="Jot Stylized Renderer (.jot)");
@@ -148,18 +146,13 @@ if __name__ == "__main__":
     register()
 
 
-#TEXBODY	{
-#	name	cactus_dancingCactus
-#	xform	{{1 0 0 0}{0 1 0 0}{0 0 1 0}{0 0 0 1}}
-#	xfdef	{ DEFINER
-#		DEFINER	{
-#			out_mask	1
-#			inputs	{ }
-#			} }
-#	color	{1 1 1}
-#	mesh_data_file	{ cactus_dancingCactus.sm }
-#	}
-#CREATE	{ cactus_dancingCactus
-#	}
 
-
+# TODO!
+# - Transform is being ignored on models.
+# - Add support for exporting result of modifiers.
+# - Add support for animation.
+# - Fix the camera (may be correct, but model transforms are wrong).
+# - Add support for exporting model, while keeping Jot annotation.
+# - Test with a riggeed and animated character.
+# - Learn how to use Jot. :)
+# - Compile the damn thing on 64bit Linux. :P
